a13327f262abf2b160d152eca66cdad0279d5d3b
Carolina <astridc.lezamam@konradlorenz.edu.co>
2020-02-15 17:00:04 -0500

    alert login
    alert login (it still doesn't work)

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,8 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+@Component({
+  selector: 'tab1.page',
+  templateUrl: 'tab1.page.html',
+})
 @NgModule({
   imports: [
     IonicModule,
@@ -16,4 +22,17 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+  constructor(public alertController: AlertController) {}
+
+  async login() {
+    const alert = await this.alertController.create({
+      header: 'LOGIN',
+      subHeader: 'BIENVENIDO',
+      message: 'Ya puedes conectarte con tus amigos ',
+      buttons: ['CONTINUAR']
+    });
+
+    await alert.present();
+  }
+}
