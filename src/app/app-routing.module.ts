9cd9eff6aeba1d8489f103d3dc3e306e52d4ba1f
Astrid Carolina Lezama <carolezama17@gmail.com>
2020-02-16 18:41:32 -0500

    routing login
    redirect the login to the menu and from the registry to the login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,26 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'register',
     loadChildren: () => import('./tab1/register/register.module').then( m => m.RegisterPageModule)
   },
   {
     path: 'login',
     loadChildren: () => import('./tab1/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'tab1',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'tab3',
+    loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
   }
 
 ];
--- src/app/tab1/login/login.module.ts
+++ src/app/tab1/login/login.module.ts
@@ -14,6 +14,7 @@ import { LoginPage } from './login.page';
     FormsModule,
     IonicModule,
     LoginPageRoutingModule
+    
   ],
   declarations: [LoginPage]
 })
